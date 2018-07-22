d1696dfb439466ee6a7258fe2860647ac2eebe12
Massimo Scola <lucerne@bluewin.ch>
2018-07-22 22:40:24 +0200

    added a proper .equals() and .hash() in the Name utility class

--- src/main/models/utility/Name.java
+++ src/main/models/utility/Name.java
@@ -66,6 +66,28 @@ public class Name implements Comparable<Name> {
         return 0;
     }
 
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        if (!super.equals(object)) return false;
+
+        Name name = (Name) object;
+
+        if (salutation != name.salutation) return false;
+        if (!firstName.equals(name.firstName)) return false;
+        if (!lastName.equals(name.lastName)) return false;
+        return DOB.equals(name.DOB);
+    }
+
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + salutation.hashCode();
+        result = 31 * result + firstName.hashCode();
+        result = 31 * result + lastName.hashCode();
+        result = 31 * result + DOB.hashCode();
+        return result;
+    }
+
     @Override
     public String toString() {
         return salutation + firstName + " " + lastName + " " + DOB;
